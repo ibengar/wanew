d0160d19e7c10138b25cdf98840414e4cc10ec60
ibengar <56824876+ibengar@users.noreply.github.com>
2022-11-21 14:44:42 +0700

    Update info-sc.js

--- plugins/info-sc.js
+++ plugins/info-sc.js
@@ -1,4 +1,4 @@
-let handler = m => m.reply('Hai kak lagi nyari sc yaa? \n\n\n_*https://wa.me/6283854551575?text=Assalamualaikum%20Bang%20Bagi%20sc%20nya%20dong?*_')
+let handler = m => m.reply('Hai kak lagi nyari sc yaa? \n\n\n_*https://wa.me/6281257172080?text=Assalamualaikum%20Bang%20Bagi%20sc%20nya%20dong?*_')
 
 handler.customPrefix = /sc|Sc|Script|script/i
 handler.command = new RegExp
